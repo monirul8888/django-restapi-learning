489f03e71c094afafae2b878da44f5c2e8825834
Monirul <monirul429588@gmail.com>
2026-05-03 15:17:10 +0600

    Updateed

--- djangoapi_ii/api_ii/serializer.py
+++ djangoapi_ii/api_ii/serializer.py
@@ -12,5 +12,14 @@ class StudentSerializer(serializers.Serializer):
 
         return Student.objects.create(**validated_data)
     
+    def update(self, instance, validated_data):
+        instance.student_name = validated_data.get("student_name", instance.student_name)
+        instance.student_id = validated_data.get("student_id", instance.student_id)
+        instance.student_dept = validated_data.get("student_dept", instance.student_dept)
+        
+
+        instance.save()
+        return instance
+    
 
     
--- djangoapi_ii/api_ii/views.py
+++ djangoapi_ii/api_ii/views.py
@@ -49,6 +49,33 @@ def insert(request):
         
         json_data = JSONRenderer().render(serializer.errors)
         return HttpResponse(json_data, content_type = "application/json")
+    
+
+    if request.method == "PUT":
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+
+        id = python_data.get("id")
+        student = Student.objects.get(id=id)
+        serializer = StudentSerializer(student, data=python_data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            res = {"msg" : "Data Updated Successfully"}
+
+            json_data = JSONRenderer().render(res)
+
+            return HttpResponse(json_data, content_type = "application/json")
+        
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type = "application/json")
+
+
+
+
+
+
         
 
 
