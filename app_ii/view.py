6fafa5298d6c73ee37dad39f91e8667b35696a92
monirul8888 <monirul429588@gmail.com>
2026-05-04 11:11:52 +0600

    Data Inserted Successfully

--- app_ii/view.py
+++ app_ii/view.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-url = "http://127.0.0.1:8000/students/"
+url = "http://127.0.0.1:8000/insert/"
 
 response = requests.get(url=url)
 
--- djangoapi_ii/api_ii/views.py
+++ djangoapi_ii/api_ii/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 
 from rest_framework.response import Response
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def insert(request, pk=None):
     if request.method == "GET":
 
@@ -20,6 +20,15 @@ def insert(request, pk=None):
         serializer = StudentSerializer(student, many= True)
 
         return Response(serializer.data)
+    
+    if request.method == "POST":
+        serializer = StudentSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response({"msg" : "Data Inserted Successfully"})
+        return Response(serializer.errors)
 
 
 
