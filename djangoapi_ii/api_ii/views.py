0c3ab1609609553d67e9a9255cf1bfba678bb68d
monirul8888 <monirul429588@gmail.com>
2026-05-04 11:46:10 +0600

    Partial and Full Data Updated Successfully

--- djangoapi_ii/api_ii/views.py
+++ djangoapi_ii/api_ii/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 
 from rest_framework.response import Response
 
-@api_view(["GET", "POST"])
+@api_view(["GET", "POST", "PUT", "PATCH"])
 def insert(request, pk=None):
     if request.method == "GET":
 
@@ -29,6 +29,34 @@ def insert(request, pk=None):
 
             return Response({"msg" : "Data Inserted Successfully"})
         return Response(serializer.errors)
+    
+
+    if request.method == "PUT":
+        id = pk
+
+        st_id = Student.objects.get(pk=id)
+
+        serializer = StudentSerializer(st_id, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg" : "Data Updated Successfully"})
+        
+        return Response(serializer.errors)
+            
+    if request.method == "PATCH":
+        id = pk
+
+        st_id = Student.objects.get(pk=id)
+
+        serializer = StudentSerializer(st_id, data = request.data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg" : "Partial Data Updated Successfully"})
+        
+        return Response(serializer.errors)
+            
+
+
 
 
 
